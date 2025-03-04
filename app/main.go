7240b6c1f2d378bb1f7dbf115fcbfdc038dd5866
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-03-04 13:29:18 +0530

    feat: catch command not found

--- app/main.go
+++ app/main.go
@@ -13,5 +13,10 @@ func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Println("Something went wrong")
+	}
+
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }
