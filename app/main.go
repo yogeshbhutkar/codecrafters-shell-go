d5fb9126fde784c85784c7ca80a2aa96f64ce6e2
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-03-04 14:01:03 +0530

    feat: add exit built-in

--- app/main.go
+++ app/main.go
@@ -4,21 +4,43 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
 func main() {
-	for {
-		fmt.Fprint(os.Stdout, "$ ")
+	fmt.Fprint(os.Stdout, "$ ")
 
-		// Wait for user input
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		if err != nil {
-			fmt.Println("Something went wrong")
+	// Wait for user input
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Println("Something went wrong")
+	}
+	command = command[:len(command)-1]
+
+	// Split the command into words.
+	args := strings.Fields(command)
+
+	switch args[0] {
+	case "exit":
+		// if the status_code is not provided, exit with the code 0.
+		if len(args) == 1 {
+			os.Exit(0)
 		}
 
-		fmt.Println(command[:len(command)-1] + ": command not found")
+		status_code, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Printf("Invalid argument %s", args[1])
+			os.Exit(0)
+		}
+		os.Exit(status_code)
+	default:
+		fmt.Println(command + ": command not found")
 	}
+
+	// Create a REPL.
+	main()
 }
